83a2a1697b988ae8f93ffb42253130b2627d2191
Puru Agarwal <puru.agar99@gmail.com>
2021-06-06 23:17:56 +0530

    Started with Exception Handling.

--- src/main/java/com/example/shuttleapi/registration/RegistrationService.java
+++ src/main/java/com/example/shuttleapi/registration/RegistrationService.java
@@ -66,8 +66,6 @@ public class RegistrationService
         }
 
         return "Incorrect Password";
-
-//        return appUserService.signIn(email, password);
     }
 
     public String logout(String email)
